968d01134fa94b307129afa42a053740b9c1e7c0
Jenny Johannesson <jennyjohannesson96@gmail.com>
2022-02-17 14:08:57 +0100

    you can now switch between two scenes with a button

--- Assets/Script/SwapScene.cs
+++ Assets/Script/SwapScene.cs
@@ -2,19 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class SwapScene : MonoBehaviour
 {
-    public Button swap;
-    public List<GameObject> test;
-    private void Start()
-    {
-        swap.onClick.AddListener(ButtonClicked);
-    }
+    //public Button swap;
+    //public List<GameObject> test;
+    //private void Start()
+    //{
+    //    swap.onClick.AddListener(ButtonClicked);
+    //}
     private void ButtonClicked()
     {
         Debug.Log("PRESSED");
-        SceneTransitionManager.Instance.GoToScene("MapScene", test);
+       // SceneTransitionManager.Instance.GoToScene("MapScene", test);
 
+    }
+
+    public void Swap(int ID)
+    {
+        SceneManager.LoadScene(ID);
     }
 }
