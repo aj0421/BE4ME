8ffd6bbdf36a3ff3f923994511db2b3c3cca420f
Jenny Johannesson <jennyjohannesson96@gmail.com>
2022-02-23 11:54:07 +0100

    build test, button disable if not near object

--- Assets/Script/CollisionDetection.cs
+++ Assets/Script/CollisionDetection.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CollisionDetection : MonoBehaviour
 {
     #region Variable
+    private Button button;
     #endregion
 
     #region Method
+    private void Start()
+    {
+        if(button == null)
+        {
+            button = GameObject.Find("ButtonHandler").GetComponentInChildren<Button>();
+        }
+        else
+        {
+            Debug.Log("Button not found");
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Character"))
         {
             other.GetComponent<MeshRenderer>().material.color = Color.red;
+            button.enabled = true;
         }
     }
 
@@ -21,6 +35,7 @@ public class CollisionDetection : MonoBehaviour
         if (other.gameObject.CompareTag("Character"))
         {
             other.GetComponent<MeshRenderer>().material.color = Color.blue;
+            button.enabled = false;
         }
     }
     #endregion
