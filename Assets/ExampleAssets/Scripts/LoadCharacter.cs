8ca2f2e5048af7bee2d915142d086f5a84a74763
Jenny Johannesson <jennyjohannesson96@gmail.com>
2022-03-03 15:39:52 +0100

    Can now select a character and it will be importet to the gamescene

--- Assets/ExampleAssets/Scripts/LoadCharacter.cs
+++ Assets/ExampleAssets/Scripts/LoadCharacter.cs
@@ -6,12 +6,21 @@ public class LoadCharacter : MonoBehaviour
 {
     public GameObject[] characters;
     public Transform spawnLocation;
+    public Transform parent;
+
+    private Vector3 scaleChange;
+    private Vector3 positionChange;
 
     private void Start()
     {
         int currentCharacter = PlayerPrefs.GetInt("selectedCharacter");
         string name = PlayerPrefs.GetString("name");
         GameObject characterPrefab = characters[currentCharacter];
-        GameObject prefabClone = Instantiate(characterPrefab, spawnLocation.position, Quaternion.identity);
+        GameObject prefabClone = Instantiate(characterPrefab, parent.position, parent.rotation);
+        scaleChange = new Vector3(10, 30, 10);
+        positionChange = new Vector3(0, 20, 0);
+        prefabClone.transform.localScale += scaleChange;
+        prefabClone.transform.position += positionChange;
+        prefabClone.transform.SetParent(parent);
     }
 }
