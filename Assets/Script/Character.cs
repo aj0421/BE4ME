68291df76d699f6292c8d86787dc6304652f0676
Emma Andersen <emmasophieandersen1@gmail.com>
2022-03-10 15:44:15 +0100

    Started on logic for individual quizzes for each character
    On going...

---
+++ Assets/Script/Character.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    #region Variables
+
+    public int ID;
+
+    public string characterName;
+
+    #endregion
+}
--- Assets/Script/Quiz/AnswerClass.cs
+++ Assets/Script/Quiz/AnswerClass.cs
@@ -7,9 +7,22 @@ public class AnswerClass : MonoBehaviour
     #region Variable
     public bool isCorrect = false;
     public QuizManager quizManager;
+    public CharacterManager characterManager;
+
+    private GameObject currentCharacter;
     #endregion
 
     #region Method
+
+    private void Awake()
+    {
+        for (int i = 0; i < characterManager.characterArray.Length; i++)
+        {
+            currentCharacter = characterManager.characterArray[i];
+        }
+        Debug.Log("current c: " + currentCharacter.GetComponent<Character>().characterName);
+    }
+
     public void Answer()
     {
         if (isCorrect)
