4cb0f524967ec02de77fd85df67b478971db4bd2
Emma Andersen <emmasophieandersen1@gmail.com>
2022-03-21 20:20:08 +0100

    Added countdown timer to timemachine screen

--- Assets/Script/TimeMachine/TimeMachine.cs
+++ Assets/Script/TimeMachine/TimeMachine.cs
@@ -1,19 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeMachine : MonoBehaviour
 {
     public CameraShake CameraShake;
-    public float duration = 5f;
+    public Text countdownText;
+    public float duration = 6.0f;
     public float magnitude = 0.3f;
+
+    private float counter;
 
     public void Start()
     {
+        counter = 6;
+
         if(CameraShake != null)
         {
-            Debug.Log("CameraShake Start: Start is being called.");
             StartCoroutine(CameraShake.Shake(duration, magnitude));
+        }
+    }
+
+    public void Update()
+    {
+        if(counter > 0)
+        {
+            counter -= Time.deltaTime;
+            countdownText.text = "" + Mathf.Ceil(counter);
+        }
+        else
+        {
+            countdownText.text = "" + 0;
         }
     }
 }
