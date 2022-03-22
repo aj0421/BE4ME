0f5b579b38f0d5f143119eaa1cf2783e1352bf74
Emma Andersen <emmasophieandersen1@gmail.com>
2022-03-22 11:38:47 +0100

    Player can choose a name in character selction, and when going to mapscene this name is showing in the upper left corner

--- Assets/Script/Player/LoadCharacter.cs
+++ Assets/Script/Player/LoadCharacter.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LoadCharacter : MonoBehaviour
 {
     public GameObject[] characters;
     public Transform spawnLocation;
     public Transform parent;
+    public Text savedPlaterText;
 
     private Vector3 scaleChange;
     private Vector3 positionChange;
@@ -22,5 +24,6 @@ public class LoadCharacter : MonoBehaviour
         prefabClone.transform.localScale += scaleChange;
         prefabClone.transform.position += positionChange;
         prefabClone.transform.SetParent(parent);
+        savedPlaterText.text = name;
     }
 }
