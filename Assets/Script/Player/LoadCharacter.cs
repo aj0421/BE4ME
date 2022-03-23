e76c4c10cdc572698021a5ac47a097ba75f5f53b
Emma Andersen <emmasophieandersen1@gmail.com>
2022-03-23 10:56:47 +0100

    Started on implementing walk animation in mapscene
    Need Jenny to test this

--- Assets/Script/Player/LoadCharacter.cs
+++ Assets/Script/Player/LoadCharacter.cs
@@ -10,20 +10,45 @@ public class LoadCharacter : MonoBehaviour
     public Transform parent;
     public Text savedPlaterText;
 
+    private Animator playerAnimator;
     private Vector3 scaleChange;
     private Vector3 positionChange;
+    private GameObject characterPrefab;
+    private GameObject prefabClone;
+    private int currentCharacter;
+    private string playerName;
 
     private void Start()
     {
-        int currentCharacter = PlayerPrefs.GetInt("selectedCharacter");
-        string name = PlayerPrefs.GetString("name");
-        GameObject characterPrefab = characters[currentCharacter];
-        GameObject prefabClone = Instantiate(characterPrefab, parent.position, parent.rotation);
+        currentCharacter = PlayerPrefs.GetInt("selectedCharacter");
+        playerName = PlayerPrefs.GetString("name");
+        savedPlaterText.text = playerName;
+
+        characterPrefab = characters[currentCharacter];
+        prefabClone = Instantiate(characterPrefab, parent.position, parent.rotation);
         scaleChange = new Vector3(20, 20, 20);
         positionChange = new Vector3(0, 20, 0);
         prefabClone.transform.localScale += scaleChange;
         prefabClone.transform.position += positionChange;
         prefabClone.transform.SetParent(parent);
-        savedPlaterText.text = name;
+
+        //Animation
+        playerAnimator = prefabClone.GetComponent<Animator>();
+        IsWalking();
+    }
+
+    private bool IsWalking()
+    {
+        //TODO: Check if player is moving then return true else return false
+        
+        Debug.Log("LOAD CHARACTER IsWalking: Player " + prefabClone.name + ", player animator: " + playerAnimator);
+        
+        if(playerAnimator != null)
+        {
+            playerAnimator.Play("WalkForward");
+            Debug.Log("Playing WalkForward");
+        }
+        
+        return true;
     }
 }
