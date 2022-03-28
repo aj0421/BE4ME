7b2089657ff4e1c80909fa47a50e42d7dcf76ea7
Emma Andersen <emmasophieandersen1@gmail.com>
2022-03-28 14:21:10 +0200

    xp-logic implemented with a tiny particle effect when the bar is filling

---
+++ Assets/Script/Player/XpBar.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class XpBar : MonoBehaviour
+{
+    public float fillSpeed = 0.5f;
+
+    private Slider slider;
+    private ParticleSystem particles;
+    private float targetXP = 0f;
+
+    private void Awake()
+    {
+        slider = gameObject.GetComponent<Slider>();
+        particles = GameObject.Find("XpParticles").GetComponent<ParticleSystem>();
+    }
+
+    private void Start()
+    {
+        IncrementXP(0.75f);
+    }
+
+    private void Update()
+    {
+        if(slider.value < targetXP)
+        {
+            slider.value += fillSpeed * Time.deltaTime;
+            if (!particles.isPlaying)
+            {
+                particles.Play();
+            }
+        }
+        else
+        {
+            particles.Stop();
+        }
+    }
+
+    public void IncrementXP(float newXP)
+    {
+        targetXP = slider.value + newXP;
+    }
+}
